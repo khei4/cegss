b04db687e1277feb7087c487d4770ce237db1bbc
khei4 <kk.asano.luxy@gmail.com>
2023-06-28 17:29:47 +0900

    add empty pass

--- main.cpp
+++ main.cpp
@@ -7,9 +7,34 @@
 #include "liboai.h"
 
 // from llvm
-#include "llvm/ADT/Triple.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 
-int main() {
-  liboai::OpenAI oai;
-  smt::solver_print_stats(std::cerr);
+using namespace llvm;
+
+static bool optimize_function(llvm::Function &F, DominatorTree &DT,
+                              TargetLibraryInfoWrapperPass &TLI) {
+  return false;
 }
+namespace {
+struct SuperoptimizerPass : PassInfoMixin<SuperoptimizerPass> {
+  PreservedAnalyses run(llvm::Function &F, FunctionAnalysisManager &FAM) {
+    // TargetLibraryInfo &TLI = FAM.getResult<TargetLibraryAnalysis>(F);
+    PreservedAnalyses PA;
+    PA.preserveSet<CFGAnalyses>();
+
+    if (F.isDeclaration())
+      return PA;
+
+    DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+
+    TargetLibraryInfoWrapperPass TLI(Triple(F.getParent()->getTargetTriple()));
+    optimize_function(F, DT, TLI);
+    return PA;
+  }
+};
+
+} // namespace
